f4b2d72721aa09bc944a4133fcfabac57c9e1855
John Jardine <john@gprime.net>
2021-06-09 19:55:51 -0400

    Fix bugs with other coins. Allow PCW V1/V2 easy config

--- bot/riskmoonlib.js
+++ bot/riskmoonlib.js
@@ -5,6 +5,8 @@ const PANCAKESWAP_FACTORY_ADDR_V2 = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"
 const ADDRESS_BNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 const ADDRESS_RISKMOON = "0xa96f3414334F5A0A529ff5d9D8ea95f42147b8C9";
 const ADDRESS_USDT = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+const ENABLE_PANCAKESWAP_V1 = true;
+const ENABLE_PANCAKESWAP_V2 = true;
 const BigNumber = require("bignumber.js");
 const Web3 = require("web3");
 const web3 = new Web3("https://bsc-dataseed1.binance.org:443");
@@ -68,31 +70,50 @@ class RiskmoonPrice {
     this.riskmoonDecimals = await riskmoonContract.methods
       .decimals()
       .call();
-      this.contractPairs = [
-        await this.getContractPair(this.pancakeswapFactoryV1, ADDRESS_RISKMOON, ADDRESS_BNB), // index 0: PCS V1, pairA = RISKMOON/BNB
-        await this.getContractPair(this.pancakeswapFactoryV2, ADDRESS_RISKMOON, ADDRESS_BNB), // index 1: PCS V2, pairA = RISKMOON/BNB
-        await this.getContractPair(this.pancakeswapFactoryV1, ADDRESS_BNB, ADDRESS_USDT), // index 3: PCS V1, pairB = BNB/USDT
-        await this.getContractPair(this.pancakeswapFactoryV2, ADDRESS_BNB, ADDRESS_USDT), // index 4: PCS V2, pairB = BNB/USDT
-      ];
+      this.contractPairsA = [];
+      this.contractPairsB = [];
+
+      if (ENABLE_PANCAKESWAP_V1) {
+        // PCS V1, pairA = RISKMOON/BNB
+        this.contractPairsA.push(await this.getContractPair(this.pancakeswapFactoryV1, ADDRESS_RISKMOON, ADDRESS_BNB));
+
+        // PCS V1, pairB = BNB/USDT
+        this.contractPairsB.push(await this.getContractPair(this.pancakeswapFactoryV1, ADDRESS_BNB, ADDRESS_USDT));
+      }
+      
+      if (ENABLE_PANCAKESWAP_V2) {
+        // PCS V2, pairA = RISKMOON/BNB
+        this.contractPairsA.push(await this.getContractPair(this.pancakeswapFactoryV2, ADDRESS_RISKMOON, ADDRESS_BNB));
+
+        // PCS V2, pairB = BNB/USDT
+        this.contractPairsB.push (await this.getContractPair(this.pancakeswapFactoryV2, ADDRESS_BNB, ADDRESS_USDT));
+      }
+      
   };
 
   getContractPair = async function (factory, address0, address1) {
     const pairAddress = await factory.methods
       .getPair(address0, address1)
       .call();
-    return new web3.eth.Contract(this.pancakeswapPairAbi, pairAddress);
+
+    const contract = new web3.eth.Contract(this.pancakeswapPairAbi, pairAddress);
+    const token0 = await contract.methods.token0().call();
+    contract.addressOrderReversed = token0.toLowerCase() !== address0.toLowerCase();
+    return contract;
   };
 
   // Price is reserve1/reserve0. However, sometimes we want to take the average of all of the pairs in the
   // event there are multiple liquidity pools. This helps in those cases.
-  getAveragedPriceFromReserves = function (reserveList) {
-    const reserve0 = reserveList.reduce(
-      (a, b) => a.plus(new BigNumber(b["0"])),
-      new BigNumber(0)
+  getAveragedPriceFromReserves = function (callContractAndResultList) {
+    const reserve0 = callContractAndResultList
+      .reduce(
+        (a, b) => a.plus(new BigNumber(b.result[b.contract.addressOrderReversed ? "1" : "0"])),
+        new BigNumber(0)
     );
-    const reserve1 = reserveList.reduce(
-      (a, b) => a.plus(new BigNumber(b["1"])),
-      new BigNumber(0)
+    const reserve1 = callContractAndResultList
+      .reduce(
+        (a, b) => a.plus(new BigNumber(b.result[b.contract.addressOrderReversed ? "0" : "1"])),
+        new BigNumber(0)
     );
     return reserve1.dividedBy(reserve0);
   };
@@ -100,20 +121,21 @@ class RiskmoonPrice {
   // web3.eth.BatchRequest allows us to batch requests, but each of the requests
   // have their own callback and return individually. It makes it a little hard to manage like this.
   // This is just a Promise that returns the entire result once they've all completed.
-  batchCalls = function (calls) {
+  batchCalls = function (callAndContractList) {
     return new Promise((resolve, reject) => {
-      let operations = calls.map((c) => ({
-        call: c,
+      let operations = callAndContractList.map((c) => ({
+        call: c.call,
+        contract: c.contract,
         completed: false,
         result: null,
       }));
 
-      const callback = function (call, error, response) {
+      const callback = function (callAndContract, error, response) {
         if (error) {
           reject(error);
         }
 
-        const currentOperation = operations.find((c) => c.call === call);
+        const currentOperation = operations.find((c) => c.call === callAndContract.call);
         currentOperation.completed = true;
         currentOperation.result = response;
 
@@ -123,8 +145,8 @@ class RiskmoonPrice {
       };
 
       let batch = new web3.eth.BatchRequest();
-      calls.forEach((call) => {
-        batch.add(call.call.request((e, r) => callback(call, e, r)));
+      callAndContractList.forEach((cc) => {
+        batch.add(cc.call.call.request((e, r) => callback(cc, e, r)));
       });
 
       batch.execute();
@@ -132,21 +154,19 @@ class RiskmoonPrice {
   };
 
   getLatestPrice = async function () {
-    const reservesResults = await this.batchCalls(
-      this.contractPairs.map((cp) => cp.methods.getReserves())
+    const reservesResultsA = await this.batchCalls(
+      this.contractPairsA.map((cp) => ({call: cp.methods.getReserves(), contract: cp }))
+    );
+
+    const reservesResultsB = await this.batchCalls(
+      this.contractPairsB.map((cp) => ({call: cp.methods.getReserves(), contract: cp }))
     );
 
     // Calculate average price for Riskmoon/BNB pair from reserves for PCS V1/V2
-    const pairPriceA = this.getAveragedPriceFromReserves([
-      reservesResults[0].result,
-      reservesResults[1].result,
-    ]);
+    const pairPriceA = this.getAveragedPriceFromReserves(reservesResultsA);
 
     // Calculate average price for BNB/USDT pair from reserves for PCS V1/V2
-    const pairPriceB = this.getAveragedPriceFromReserves([
-      reservesResults[2].result,
-      reservesResults[3].result,
-    ]);
+    const pairPriceB = this.getAveragedPriceFromReserves(reservesResultsB);
 
     // Multiply pair A by pair B to get the USD value
     let price = pairPriceA.multipliedBy(pairPriceB);
